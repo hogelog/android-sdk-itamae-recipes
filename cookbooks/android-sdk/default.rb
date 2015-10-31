ab7fd47fea9a1ab2226b6bf8e0217083b05bec8f
hogelog <konbu.komuro@gmail.com>
2015-10-31 20:37:22 +0900

    Update android sdk packages

--- cookbooks/android-sdk/default.rb
+++ cookbooks/android-sdk/default.rb
@@ -1,4 +1,5 @@
 VERSION = "r24.3.4"
+SDK_PACKAGES = "platform-tools,build-tools-23.0.2,android-23,sys-img-x86-android-23,extra-android-m2repository,extra-google-m2repository"
 
 directory "/opt/src"
 
@@ -10,5 +11,15 @@ execute "download sdk" do
     mv android-sdk-linux android-sdk_#{VERSION}
     ln -s /opt/src/android-sdk_#{VERSION} /opt/android-sdk
   EOM
-  not_if "/opt/src/android-sdk_#{VERSION}"
+  not_if "test -e /opt/src/android-sdk_#{VERSION}"
+end
+
+execute "update android sdk" do
+  command <<-EOM
+    export ANDROID_HOME=/opt/android-sdk
+    export ANDROID_SDK=/opt/android-sdk
+    export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
+    echo y | android update sdk --no-ui --all --filter #{SDK_PACKAGES}
+  EOM
+  not_if "test -e /opt/android-sdk/platform-tools"
 end
